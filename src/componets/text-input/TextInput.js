c15ec7ade7bd97b609db12811bca07c3041a3eb8
stanislav7766 <stanislavtihonov5@gmail.com>
2020-09-12 18:59:33 +0300

    changed textInput component style

--- src/componets/text-input/TextInput.js
+++ src/componets/text-input/TextInput.js
@@ -2,12 +2,21 @@ import React, {useState} from 'react';
 import {View, TextInput} from 'react-native';
 import {styleTextInput, styleContainer, styleUnderline} from './styles';
 
-const Input = ({value, onChangeText, style, onSubmitEditing, placeholder, openModal}) => {
+const Input = ({
+  value,
+  autoCompleteType,
+  secureTextEntry,
+  onChangeText,
+  style,
+  onSubmitEditing,
+  placeholder,
+  openModal,
+}) => {
   const [isFocused, setIsFocused] = useState(false);
 
   const handleFocus = () => {
     setIsFocused(true);
-    openModal();
+    openModal && openModal();
   };
   const handleBlur = () => {
     setIsFocused(false);
@@ -16,6 +25,8 @@ const Input = ({value, onChangeText, style, onSubmitEditing, placeholder, openMo
   return (
     <View style={styleContainer}>
       <TextInput
+        secureTextEntry={secureTextEntry || false}
+        autoCompleteType={autoCompleteType || 'off'}
         style={(styleTextInput, {color: style.textColor})}
         placeholder={placeholder}
         placeholderTextColor={style.placeholderColor}
--- src/componets/text-input/styles.js
+++ src/componets/text-input/styles.js
@@ -1,15 +1,12 @@
 export const styleContainer = {
   width: '100%',
-  marginTop: 10,
 };
 
 export const styleTextInput = {
   color: '#000',
-  height: 42,
-  fontSize: 17,
 };
 export const styleUnderline = {
+  marginTop: -5,
   borderBottomWidth: 3,
   borderBottomColor: '#000',
-  elevation: 16,
 };
