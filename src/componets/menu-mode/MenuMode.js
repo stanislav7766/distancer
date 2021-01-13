d3145efed692fb0d4f74c075e2b96158beb8182d
stanislav7766 <stanislavtihonov5@gmail.com>
2021-01-13 20:58:44 +0200

    added opacity on mount & removed hardcored maxHeight of scrollView

--- src/componets/menu-mode/MenuMode.js
+++ src/componets/menu-mode/MenuMode.js
@@ -1,18 +1,25 @@
 import React from 'react';
-import {ScrollView} from 'react-native';
+import {ScrollView, Animated} from 'react-native';
 import Shared from './Shared';
 import NotAuthorized from './NotAuthorized';
 import {useOnIsDirectionsMode} from '../../hooks/use-directions-mode';
 import Authorized from './Authorized';
+import useEffectOpacity from '../../hooks/use-effect-opacity';
 import {styles} from './styles';
 import {useAuth} from '../../stores/auth';
 import {observer} from 'mobx-react-lite';
 
 const MenuMode = ({themeStyle, navigator}) => {
   const {authorized} = useAuth();
+  const [opacityGroup] = useEffectOpacity(400);
   useOnIsDirectionsMode({mount: false});
+
+  const opacityProps = {
+    needsOffscreenAlphaCompositing: true,
+    style: {opacity: opacityGroup},
+  };
   return (
-    <>
+    <Animated.View {...opacityProps}>
       <ScrollView showsVerticalScrollIndicator={false} style={styles.scrollView}>
         {authorized ? (
           <Authorized themeStyle={themeStyle} navigator={navigator} />
@@ -21,7 +28,7 @@ const MenuMode = ({themeStyle, navigator}) => {
         )}
         <Shared themeStyle={themeStyle} navigator={navigator} />
       </ScrollView>
-    </>
+    </Animated.View>
   );
 };
 
--- src/componets/menu-mode/styles.js
+++ src/componets/menu-mode/styles.js
@@ -1,13 +1,11 @@
 import {StatusBar, StyleSheet} from 'react-native';
-import {NAVBAR_HEIGHT, WINDOW_HEIGHT} from '../../constants/constants';
 
 const hasNotch = StatusBar.currentHeight > 24;
 const padding = 10;
 
 export const styles = StyleSheet.create({
   scrollView: {
-    maxHeight: WINDOW_HEIGHT - NAVBAR_HEIGHT - padding * 2,
-    top: NAVBAR_HEIGHT + (hasNotch ? 0 : StatusBar.currentHeight),
+    top: hasNotch ? 0 : StatusBar.currentHeight,
     marginTop: padding,
   },
 });
