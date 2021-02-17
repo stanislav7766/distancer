379618c238df5a1d3b7aee161d5bb934fd3e2332
stanislav7766 <stanislavtihonov5@gmail.com>
2021-02-17 02:13:34 +0200

    fixed app settings 'defaultScreen'

--- src/stores/app-settings/index.js
+++ src/stores/app-settings/index.js
@@ -4,18 +4,28 @@ import {DEFAULT_SCREEN, DEFAULT_THEME} from '~/constants/constants';
 import {isExist} from '~/utils/validation/helpers';
 
 export class AppSettingsStore {
-  constructor(themeStore) {
+  constructor({themeStore, appModeStore, authStore}) {
     this.themeStore = themeStore;
+    this.appModeStore = appModeStore;
+    this.authStore = authStore;
     this.theme = themeStore.theme;
+
     observe(this.themeStore, this._listenThemeStore);
     makeAutoObservable(this);
+    observe(this, this._listenDefaultScreen);
   }
 
   theme = this?.themeStore?.theme ?? DEFAULT_THEME;
   defaultScreen = DEFAULT_SCREEN;
+  _loadedSettings = false;
+
+  setLoadedSettings = () => {
+    this._loadedSettings = true;
+  };
 
   setDefaultScreen = defaultScreen => {
     this.defaultScreen = defaultScreen;
+    this.setLoadedSettings();
   };
   setTheme = theme => {
     this.themeStore.setTheme(theme);
@@ -27,6 +37,10 @@ export class AppSettingsStore {
 
   _listenThemeStore = ({name, oldValue, newValue}) => {
     name === 'theme' && this.theme === oldValue && (this.theme = newValue);
+  };
+  _listenDefaultScreen = ({name, newValue}) => {
+    if (name !== 'defaultScreen') return;
+    !this._loadedSettings && this.authStore.authorized && this.appModeStore.setAppMode(newValue);
   };
 }
 
