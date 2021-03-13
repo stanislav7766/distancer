73a5d1f983594dcab4087544f2347f21b2e98dbe
stanislav7766 <stanislavtihonov5@gmail.com>
2021-03-13 22:15:47 +0200

    moved hooks from landing to main

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import {MainMap} from '~/componets/map';
 import {AppModeModal} from '~/componets/app-mode-modal';
-import useListenSettings from '~/hooks/use-listen-settings';
-import useGpsPermissions from '~/hooks/use-gps-permissions';
 import {useRemindFillProfile} from '~/hooks/use-remind-fill-profile';
-import {useCurrentUser} from '~/hooks/use-current-user';
 import {observer} from 'mobx-react-lite';
 import {Navbar} from '~/componets/navbar';
 
 const Landing = () => {
-  useGpsPermissions();
-  useListenSettings();
   useRemindFillProfile();
-  useCurrentUser();
 
   return (
     <>
