e3cb0f1865827c13fd31e0a187308ba1f6f517e0
stanislav7766 <stanislavtihonov5@gmail.com>
2021-03-13 22:10:32 +0200

    updated app-state usage for use-gps-settings

--- src/hooks/use-gps-permissions/use-gps-permissions.js
+++ src/hooks/use-gps-permissions/use-gps-permissions.js
@@ -1,15 +1,19 @@
-import {useEffect} from 'react';
+import {useEffect, useCallback} from 'react';
 import {BackHandler} from 'react-native';
 import {askGpsPermissions} from '~/utils/ask-permissions';
 import Toast from 'react-native-simple-toast';
 import {getLocaleStore} from '~/stores/locale';
+import {useAppState} from '~/stores/app-state';
 
 const {papyrusify} = getLocaleStore();
 
 const useGpsPermissions = () => {
-  useEffect(() => {
-    askGpsPermissions().then(
-      res => {
+  const {setAllowUpdate} = useAppState();
+
+  const ask = useCallback(() => {
+    setAllowUpdate(false);
+    askGpsPermissions()
+      .then(res => {
         if (res) {
           Toast.show(papyrusify('permissions.message.gpsGranted'));
           return;
@@ -17,13 +21,22 @@ const useGpsPermissions = () => {
         Toast.show(papyrusify('permissions.message.gpsDenies'));
         Toast.show(papyrusify('permissions.message.gpsAllow'));
         BackHandler.exitApp();
-      },
-      _err => {
+      })
+      .catch(_err => {
         Toast.show(papyrusify('common.message.errorOccurred'));
         BackHandler.exitApp();
-      },
-    );
-  }, []);
+      })
+      .finally(() => {
+        setAllowUpdate(true);
+      });
+  }, [setAllowUpdate]);
+
+  useEffect(() => {
+    const timeout = setTimeout(ask, 1000);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [ask]);
 };
 
 export default useGpsPermissions;
